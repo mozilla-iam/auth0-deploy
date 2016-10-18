6aa353f330198231020f44d6d13395dd72c0baf0
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2016-10-18 16:45:40 -0700

    Add support for DuoSecurity MFA

---
+++ rules/duosecurity.js
@@ -0,0 +1,17 @@
+function (user, context, callback) {
+  // LDAP group okta_mfa requires MFA authentication everywhere.
+  if (user.groups && user.groups.indexOf("okta_mfa")) {
+    context.multifactor = {
+      provider: 'duo',
+      ikey: configuration.duo_ikey_mozilla,
+      skey: configuration.duo_skey_mozilla,
+      host: configuration.duo_apihost_mozilla,
+
+      // optional:
+      // Force DuoSecurity everytime this rule runs. Defaults to false.
+      // If accepted by users the cookie lasts for 30 days - i.e. 30 days MFA session (this cannot be changed)
+      ignoreCookie: false,
+    };
+  }
+  callback(null, user, context);
+}
