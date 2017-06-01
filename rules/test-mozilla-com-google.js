c0a78855b05822f41dc8dc2ececd58e1a8144ce3
Justin Dow <justin@dowhaus.com>
2017-06-01 12:53:53 -0700

    adding comments

--- rules/test-mozilla-com-google.js
+++ rules/test-mozilla-com-google.js
@@ -1,6 +1,7 @@
 function (user, context, callback) {
-  if (context.clientID !== 'q0tFB9QyFIKqPOOKvkFnHMj2VwrLjX46') return callback(null, user, context);
+  if (context.clientID !== 'q0tFB9QyFIKqPOOKvkFnHMj2VwrLjX46') return callback(null, user, context); // Google (test.mozilla.com)
 
+  // This rule simply remaps @mozilla.com e-mail addresses to @test.mozilla.com to be used with the test.mozilla.com GSuite domain.
   user.myemail = user.email.replace("mozilla.com", "test.mozilla.com");
   context.samlConfiguration = context.samlConfiguration || {};
 
