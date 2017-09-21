79d19a4b89a342da530396b95ca7381822766c08
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2017-09-21 13:49:28 -0700

    Better message with link for the user

--- rules/github_mfa.js
+++ rules/github_mfa.js
@@ -1,8 +1,13 @@
 function (user, context, callback) {
   if ((context.connection === 'github') && (!user.two_factor_authentication)) {
     console.log('GitHub user not allowed to log in because 2FA was disabled on the account: '+user.user_id);
+    var reason = 'You must setup a security device ("MFA", "2FA") for your GitHub account in order to access ' +
+      'this service. Please follow the ' +
+      '<a href="https://help.github.com/articles/securing-your-account-with-two-factor-authentication-2fa/"> ' +
+      'GitHub documentation</a> to setup the device and try logging in again.';
+
     context.redirect = {
-     url: "https://sso.mozilla.com/forbidden?reason=githubmfa"
+     url: "https://sso.mozilla.com/forbidden?reason="+encodeURIComponent(reason)
     };
     return callback(null, null, context);
   } else {
