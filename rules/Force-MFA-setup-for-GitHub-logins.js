e7b8bac3c69c23df21b946e67246d7d18ffdf472
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2018-03-28 15:49:22 -0700

    Change GitHub force MFA rule to be a global social provider force-MFA rule Add FxA to enforced MFA rules

--- rules/Force-MFA-setup-for-GitHub-logins.js
+++
@@ -1,8 +0,0 @@
-function (user, context, callback) {
-  if ((context.connection === 'github') && (!user.two_factor_authentication)) {
-    console.log('GitHub user not allowed to log in because 2FA was disabled on the account: '+user.user_id);
-    return callback(null, user, global.postError('githubrequiremfa', context));
-  } else {
-    return callback(null, user, context);
-  }
-}
