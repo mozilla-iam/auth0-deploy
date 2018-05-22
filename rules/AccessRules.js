6e8230f8e4f60fd936f3ef0f3ea2d56f19609447
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-05-22 16:42:03 -0700

    Change access expiration logic to occur after AccessRules have succeeded This involves some refactoring.
    Note that this also means:
    - accessing *any* RP will update your authoritativeGroups.lastUsed
    timestamp for that RP
    - only RPs with an expiration setting in apps.yml will enforce the
    access expiration at this time. We may enable it by default everywhere
    in the future.
    - if you are granted access to an RP, but never use the access, your
    timestamp in lastUsed is never set. This means you can keep that access
    and it will NOT expire until you first login to the RP, and after the
    lastUsed attribute expires. This is a known issue that we're accepting
    as expiration of access is yet another additional level of security, not the
    primary decision for access.
    
    We may be able to fix the issue eventually, if groups get tied to roles,
    which themselves must be tied to an RP-subject for example (so that we
    know why RPs you have access to, and thus set your lastUsed timestamp as
    soon as you get access, regardless of you logging in or not)

--- rules/AccessRules.js
+++ rules/AccessRules.js
@@ -1,7 +1,7 @@
 function (user, context, callback) {
   var rules_url = 'https://cdn.sso.mozilla.com/apps.yml'; //S3 bucket with CFN
   var groups = user.groups || [];
-  
+
   // Check if array A has any occurence from array B
   function array_in_array(A, B) {
     var found = A.some(
@@ -12,18 +12,29 @@ function (user, context, callback) {
     });
     return found;
   }
+  function access_granted(a, b, c) {
+    global.updateAccessExpiration();
+    return callback(a, b, c);
+  }
+
+  function access_denied(a, b, c) {
+    return callback(a, b, c);
+  }
+
   // Process the access cache decision
-  function access_decision(access_rules, callback) {
+  function access_decision(access_rules) {
     for (var i=0;i<access_rules.length;i++) {
       var app = access_rules[i].application;
+
       //Handy for quick testing in dev (overrides access rules)
       //var app = {'client_id': 'pCGEHXW0VQNrQKURDcGi0tghh7NwWGhW', // This is testrp social-ldap-pwless
       //           'authorized_users': ['gdestuynder@mozilla.com'],
-      //           'authorized_groups': ['okta_mfa']
+      //           'authorized_groups': ['okta_mfa'],
+      //           'expire_access_when_unused_after': 86400
       //          };
 
       if (app.client_id && (app.client_id.indexOf(context.clientID) >= 0)) {
-        // The follow expiration check MUST always run first
+        // Note that the expiration check MUST always run first
         // Check if the user access to the RP has expired due to ExpirationOfAccess
         if ((app.expire_access_when_unused_after !== undefined) && (app.expire_access_when_unused_after > 0)) {
           user.app_metadata = user.app_metadata || {};
@@ -34,51 +45,48 @@ function (user, context, callback) {
                 // Find the delta for this user and see if access should have expired
                 var lastUsed_ts = new Date(user.app_metadata.authoritativeGroups[index].lastUsed).getTime();
                 var delta = new Date().getTime() - lastUsed_ts;
-                // If not expired, let the user in
-                if (delta < app.expire_access_when_unused_after) {
-                  return callback(null, user, context);
-                } else {
+                // Access expired?
+                if (delta > app.expire_access_when_unused_after) {
                     // Do not allow the user in, no matter what other access has been set
                     console.log("Access denied to "+context.clientID+" for user "+user.email+" ("+user.user_id+") - access has expired");
-                    return callback(null, user, global.postError('accesshasexpired', context));
+                    return access_denied(null, user, global.postError('accesshasexpired', context));
                 }
                 break;
               }
             }
           }
-          return callback(null, user, context);
         }
 
         // XXX this authorized_users SHOULD BE REMOVED as it's unsafe. USE GROUPS.
         // XXX This needs to be fixed in the dashboard first
-
         // Empty users or groups (length == 0) means no access in the dashboard apps.yml world
         if (app.authorized_users.length === app.authorized_groups.length === 0) {
           console.log("Access denied to "+context.clientID+" for user "+user.email+" ("+user.user_id+") - this app denies ALL users and ALL groups");
-          return callback(null, user, global.postError('notingroup', context));
+          return access_denied(null, user, global.postError('notingroup', context));
         }
+
         // Check if the user is authorized to access
         if ((app.authorized_users.length > 0 ) && (app.authorized_users.indexOf(user.email) >= 0)) {
-          return callback(null, user, context);
+          return access_granted(null, user, context);
         // Same dance as above, but for groups
         } else if ((app.authorized_groups.length > 0) && array_in_array(app.authorized_groups, groups)) {
-          return callback(null, user, context);
+          return access_granted(null, user, context);
         }
 
         console.log("Access denied to "+context.clientID+" for user "+user.email+" ("+user.user_id+") - not in authorized group or not an authorized user");
-        return callback(null, user, global.postError('notingroup', context));
+        return access_denied(null, user, global.postError('notingroup', context));
       } // correct client id
     } // for loop
     // We matched no rule, access is granted
-    return callback(null, user, context);
+    return access_granted(null, user, context);
   }
-  
+
   // Fetch the apps.yml access rules or use cache if available
   // Note that the cache is very short lived, though it's better than nothing
   // Basically: the underlaying webtask is still running and reused,
   // thus the global namespace (`global`) is shared/still in memory and available to us
   if (global.access_rules) {
-    return access_decision(global.access_rules, callback);
+    return access_decision(global.access_rules);
   } else {
     var request = require('request');
     var YAML = require('js-yaml');
@@ -88,11 +96,11 @@ function (user, context, callback) {
       request(options, function (error, response, body) {
         if (error) throw new Error(error);
         global.access_rules = YAML.load(body).apps;
-        return access_decision(global.access_rules, callback);
+        return access_decision(global.access_rules);
       });
     } catch(e) {
       console.log('Error fetching access rules (fatal): '+e);
-      return callback(null, null, context);
+      return access_denied(null, null, context);
     }
   }
 }
--- rules/Global-Function-Declarations.js
+++ rules/Global-Function-Declarations.js
@@ -3,6 +3,37 @@ function (user, context, callback) {
   // This rule MUST be at the top of the rule list (FIRST) or other rules WILL FAIL
   // with a NON RECOVERABLE error, and thus LOGIN WILL FAIL FOR USERS
 
+  // updateAccessExpiration()
+  // Always returns - will attempt to update user.app_metadata.authoritativeGroups[].lastUsed timestamp
+  // for the RP/client_id we're currently trying to login to
+  if (!global.updateAccessExpiration) {
+    global.updateAccessExpiration = function updateAccessExpiration() {
+      user.app_metadata = user.app_metadata || {};
+      if (user.app_metadata.authoritativeGroups === undefined) {
+          console.log('ExpirationOfAccess: Not used here');
+          return;
+      }
+
+      var updated = false;
+      for (var index = 0;index < user.app_metadata.authoritativeGroups.length;++index) {
+        if (user.app_metadata.authoritativeGroups[index].uuid === context.clientID) {
+          user.app_metadata.authoritativeGroups[index].lastUsed = new Date();
+          updated = true;
+          break; // we're done
+        }
+      }
+      if (updated === true) {
+        auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
+          .catch(function(err) {
+          console.log('ExpirationOfAccess: Error updating app_metadata (AuthoritativeGroups) for user '+user.user_id+': '+err);
+        });
+      }
+      console.log('ExpirationOfAccess: Updated lastUsed for '+user.user_id);
+      return;
+    };
+      return;
+  }
+
   // postError(code)
   // @code string with an error code for the SSO Dashboard to display
   // @rcontext the current Auth0 rule context (passed from the rule)
@@ -11,7 +42,7 @@ function (user, context, callback) {
     global.postError = function postError(code, rcontext, prefered_connection_arg) {
       var jwt = require('jsonwebtoken');
       var prefered_connection = prefered_connection_arg || ""; // Optional arg
-      
+
       // Token is valid from 30s ago, to 1h from now
       var skey = new Buffer(configuration.jwt_msgs_rsa_skey, 'base64').toString('ascii');
       var token = jwt.sign({ code: code,
