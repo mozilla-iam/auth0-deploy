27d65b5ec167e63984fb4e6c12738d2e4a47f161
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-05-29 16:44:11 -0700

    clarify examples and settings

--- manual/social-fxa.js
+++ manual/social-fxa.js
@@ -4,11 +4,17 @@
 // Authenticator Assurance Level 2: AAL2 provides high confidence that the claimant controls an authenticator(s) bound to the subscriber’s account. Proof of possession and control of two different authentication factors is required through secure authentication protocol(s). Approved cryptographic techniques are required at AAL2 and above.
 // We'd also be fine with AAL3 of course, though FxA supports up to AAL2 at this time.
 
-// latest-dev URLs:
+// Well-known URLs:
+// Prod: https://accounts.firefox.com/.well-known/openid-configuration
+// Stage: https://accounts.stage.mozaws.net/.well-known/openid-configuration
+// Dev: https://oauth-latest.dev.lcip.org/.well-known/openid-configuration
+//
+// Example:
 // Authorization endpoint: https://oauth-latest.dev.lcip.org/v1/authorization?acr_values=AAL2
 // Token endpoint: https://oauth-latest.dev.lcip.org/v1/token
 // scopes: openid profile
-//
+// Note that as per above, using the authorization parameter `acr_values=AAL2` will enforce authenticating accounts with 2FA only
+
 function(accessToken, ctx, cb) {
   // Auth0 already verified the id_token and it's signature at this stage
   // See docs at https://developer.mozilla.org/en-US/docs/Mozilla/Tech/Firefox_Accounts/Introduction
