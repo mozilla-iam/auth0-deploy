4b854ef3dd632361bf6047651a8b0cc6078edcb6
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-06-11 09:22:01 -0700

    merge back dev manual fixes

--- rules/AccessRules.js
+++ rules/AccessRules.js
@@ -25,28 +25,28 @@ function (user, context, callback) {
   // Always returns - will attempt to update user.app_metadata.authoritativeGroups[].lastUsed timestamp
   // for the RP/client_id we're currently trying to login to
   function updateAccessExpiration() {
-    user.app_metadata = user.app_metadata || {};
-    if (user.app_metadata.authoritativeGroups === undefined) {
-        console.log('ExpirationOfAccess: Not used here');
-        return;
-    }
-
-    var updated = false;
-    for (var index = 0;index < user.app_metadata.authoritativeGroups.length;++index) {
-      if (user.app_metadata.authoritativeGroups[index].uuid === context.clientID) {
-        user.app_metadata.authoritativeGroups[index].lastUsed = new Date();
-        updated = true;
-        break; // we're done
+      user.app_metadata = user.app_metadata || {};
+      if (user.app_metadata.authoritativeGroups === undefined) {
+          console.log('ExpirationOfAccess: Not used here');
+          return;
       }
-    }
-    if (updated === true) {
-      auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
-        .catch(function(err) {
-        console.log('ExpirationOfAccess: Error updating app_metadata (AuthoritativeGroups) for user '+user.user_id+': '+err);
-      });
-    }
-    console.log('ExpirationOfAccess: Updated lastUsed for '+user.user_id);
-    return;
+
+      var updated = false;
+      for (var index = 0;index < user.app_metadata.authoritativeGroups.length;++index) {
+        if (user.app_metadata.authoritativeGroups[index].uuid === context.clientID) {
+          user.app_metadata.authoritativeGroups[index].lastUsed = new Date();
+          updated = true;
+          break; // we're done
+        }
+      }
+      if (updated === true) {
+        auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
+          .catch(function(err) {
+          console.log('ExpirationOfAccess: Error updating app_metadata (AuthoritativeGroups) for user '+user.user_id+': '+err);
+        });
+      }
+      console.log('ExpirationOfAccess: Updated lastUsed for '+user.user_id);
+      return;
   }
 
   // Process the access cache decision
