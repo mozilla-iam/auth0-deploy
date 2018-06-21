f35e40e77dc3d7f97e767b98867f76c65cbb4de3
viorelaioia <viorelaioia@gmail.com>
2018-06-21 15:34:53 +0300

    Fix flake8 and update github repo url

--- params2json.py
+++ params2json.py
@@ -1,5 +1,4 @@
 """Takes parameter store secrets and decrypts them to JSON that can be used by test-automation."""
-
 import boto3
 import json
 
@@ -17,32 +16,31 @@ def get_secret(parameter_key, is_secure=True):
 
 def dump_secrets():
     secrets = {
-      "users": {
-        "ldap": {
-          "email": get_secret('/iam/automated-test/ldap_email'),
-          "password": get_secret('/iam/automated-test/ldap_password'),
-          "secret_seed": get_secret('/iam/automated-test/ldap_secret_seed')
-        },
-        "passwordless": {
-          "email": get_secret('/iam/automated-test/passwordless_email')
-        },
-        "github": {
-          "username": get_secret('/iam/automated-test/github_email'),
-          "password": get_secret('/iam/automated-test/github_password'),
-          "secret_seed": get_secret('/iam/automated-test/github_secret_seed')
-        },
-        "google": {
-          "email": get_secret('/iam/automated-test/google_email'),
-          "password": get_secret('/iam/automated-test/google_password')
-        },
-        "fxa": {
-            "email": get_secret('/iam/automated-test/fxa_email'),
-            "password": get_secret('/iam/automated-test/fxa_password'),
-            "secret_seed": get_secret('/iam/automated-test/fxa_secret_seed')
+        "users": {
+            "ldap": {
+                "email": get_secret('/iam/automated-test/ldap_email'),
+                "password": get_secret('/iam/automated-test/ldap_password'),
+                "secret_seed": get_secret('/iam/automated-test/ldap_secret_seed')
+            },
+            "passwordless": {
+                "email": get_secret('/iam/automated-test/passwordless_email')
+            },
+            "github": {
+                "username": get_secret('/iam/automated-test/github_email'),
+                "password": get_secret('/iam/automated-test/github_password'),
+                "secret_seed": get_secret('/iam/automated-test/github_secret_seed')
+            },
+            "google": {
+                "email": get_secret('/iam/automated-test/google_email'),
+                "password": get_secret('/iam/automated-test/google_password')
+            },
+            "fxa": {
+                "email": get_secret('/iam/automated-test/fxa_email'),
+                "password": get_secret('/iam/automated-test/fxa_password'),
+                "secret_seed": get_secret('/iam/automated-test/fxa_secret_seed')
+            }
         }
-      }
     }
-
     return secrets
 
 
