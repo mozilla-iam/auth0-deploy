7dbfbaa9c596e0b959f896b457882e545a291679
Gene Wood <gene_wood@cementhorizon.com>
2018-12-11 12:24:24 -0800

    Clarify the terms used for user variables

--- rules/link-users-by-email-with-metadata.js
+++ rules/link-users-by-email-with-metadata.js
@@ -32,6 +32,11 @@ function (user, context, callback) {
   const userApiUrl = auth0.baseUrl + '/users';
   const userSearchApiUrl = auth0.baseUrl + '/users-by-email';
 
+  // currentUser : The user that just logged in, same as "user"
+  // primaryUser : The user profile that all other user profiles will be linked
+  //               to as children
+  // targetUser : The iterator that we use as we iterate over the search results
+
   request({
    url: userSearchApiUrl,
    headers: {
@@ -56,23 +61,23 @@ function (user, context, callback) {
       return callback(null, user, context);
     }
 
-    // Default to ourselves as main profile
-    var originalUser = user;
+    // Default to ourselves as the primary profile
+    var primaryUser = user;
 
-    // We have matches in Auth0, so we need to decide which account is going to be main
-    // Please see https://github.com/mozilla-iam/mozilla-iam/blob/master/docs/deratcheting-user-flows.md#user-logs-in-with-the-mozilla-iam-system-for-the-first-time 
+    // We have matches in Auth0, so we need to decide which account is going to be primary
+    // Please see https://github.com/mozilla-iam/mozilla-iam/blob/master/docs/deratcheting-user-flows.md#user-logs-in-with-the-mozilla-iam-system-for-the-first-time
     // for detailed explanation of what happens here.
-    // NOTE: No date time check is made here as the current/new user is not yet present in Auth0 database (anymore?)
+    // NOTE: No date time check is made here as the current user is not yet present in Auth0 database (anymore?)
     // thus we do not need to verify that the current profile (`user`) has a "new" `user.created_at` value. Yay!
     for (var i = 0, len = data.length; i < len; i++) {
       var targetUser = data[i];
 
-      var otherConnection = targetUser.identities[0].connection;
-      var curConnection = originalUser.identities[0].connection;
+      var targetConnection = targetUser.identities[0].connection;
+      var primaryConnection = primaryUser.identities[0].connection;
 
-      if (matchOrder[otherConnection] < matchOrder[curConnection]) {
-        console.log("Found user_id that should be main profile used for linking, according to ratcheting logic: " + targetUser.user_id);
-        originalUser = targetUser;
+      if (matchOrder[targetConnection] < matchOrder[primaryConnection]) {
+        console.log("Found user_id that should be primary profile used for linking, according to ratcheting logic: " + targetUser.user_id);
+        primaryUser = targetUser;
       }
     }
 
@@ -80,23 +85,23 @@ function (user, context, callback) {
     const provider = user.identities[0].provider;
     const providerUserId = user.identities[0].user_id;
 
-    if (originalUser.user_id === user.user_id) {
-      // The profile we're trying to link is the same as the one we're logged in as
+    if (primaryUser.user_id === user.user_id) {
+      // The primary user we're trying to link is the same as the one we're logged in as
       // This happens if the linking ratcheting logic selects the same profile and other profiles are not yet linked
-      // These will be linked the first time the user login with them instead
-      console.log("No automatic profile linking performed due to main profile matching current profile for: " + user.user_id);
+      // These will be linked the first time the user logs in with them instead
+      console.log("No automatic profile linking performed due to primary profile matching current profile for: " + user.user_id);
       return callback(null, user, context);
     }
 
-    console.log("Performing automatic profile linking: main profile: "+originalUser.user_id+" is now also main for: " + user.user_id);
+    console.log("Performing automatic profile linking: primary profile: "+primaryUser.user_id+" is now also primary for: " + user.user_id);
 
     user.app_metadata = user.app_metadata || {};
     user.user_metadata = user.user_metadata || {};
-    auth0.users.updateAppMetadata(originalUser.user_id, user.app_metadata)
-    .then(auth0.users.updateUserMetadata(originalUser.user_id, user.user_metadata))
+    auth0.users.updateAppMetadata(primaryUser.user_id, user.app_metadata)
+    .then(auth0.users.updateUserMetadata(primaryUser.user_id, user.user_metadata))
     .then(function() {
       request.post({
-        url: userApiUrl + '/' + originalUser.user_id + '/identities',
+        url: userApiUrl + '/' + primaryUser.user_id + '/identities',
         headers: {
           Authorization: 'Bearer ' + auth0.accessToken
         },
@@ -106,7 +111,7 @@ function (user, context, callback) {
             console.log("Error linking account: " + response.statusMessage);
             return callback(new Error('Error linking account: ' + response.statusMessage));
           }
-          context.primaryUser = originalUser.user_id;
+          context.primaryUser = primaryUser.user_id;
           callback(null, user, context);
       });
     })
