c1c7528ed6fb098f2f73c353b5cf171b0502bfe8
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-05-06 15:05:44 -0700

    support string-like group format: groupA"groupB"groupC"
    Note that any double quote (") in the group name will be replaced by
    &quot; prior to generating the group-string-list in order to avoid any
    group reusing our token (")

--- rules/AWS-Federated-AMR.js
+++ rules/AWS-Federated-AMR.js
@@ -4,7 +4,25 @@ function (user, context, callback) {
                    'N7lULzWtfVUDGymwDs0yDEq6ZcwmFazj', // Federated AWS CLI auth0-prod
   ];
   if (WHITELIST.indexOf(context.clientID) >= 0) {
-    context.idToken['amr'] = 'test';
+    const token = '&quot;';
+    var filtered = [];
+    console.log("Enriching id_token with amr for AWS Federated CLI");
+
+    user.groups = user.groups || [];
+
+    // always replace our token so that it cannot be bypassed by a group that would use it
+    // this is because the AWS rule/policy condition uses the `amr` value with a string match
+    // and does not know how to parse the list
+    // See also
+    // https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_condition_operators.html#Conditions_String
+    Array.prototype.forEach.call(user.groups, function(value) {
+      Array.prototype.push.apply(filtered, [value.replace('"', token)]);
+    });
+
+    // String will look like:
+    // 'amr': 'groupA"groupB"groupC"'
+    var tmp = user.groups.join('"');
+    context.idToken['amr'] = tmp;
   }
   return callback(null, user, context);
 }
