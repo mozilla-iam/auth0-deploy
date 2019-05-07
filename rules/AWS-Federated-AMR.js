f3d0bc25605e326ed952eed896227095968e316f
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-05-07 08:42:03 -0700

    simplify rule code, we found out that AWS will accept a list

--- rules/AWS-Federated-AMR.js
+++ rules/AWS-Federated-AMR.js
@@ -4,26 +4,15 @@ function (user, context, callback) {
                    'N7lULzWtfVUDGymwDs0yDEq6ZcwmFazj', // Federated AWS CLI auth0-prod
   ];
   if (WHITELIST.indexOf(context.clientID) >= 0) {
-    const token = '&quot;';
-    var filtered = "";
-    var tmp = "";
     console.log("Enriching id_token with amr for AWS Federated CLI");
-
-    user.groups = user.groups || [];
-
-    // always replace our token so that it cannot be bypassed by a group that would use it
-    // this is because the AWS rule/policy condition uses the `amr` value with a string match
-    // and does not know how to parse the list
-    // See also
+    // Amazon will read in the id token's `amr` list and allow you to match on policies with a string condition.
+    // See
     // https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_condition_operators.html#Conditions_String
-    // String will look like:
-    // 'amr': ''groupA""groupB""groupC"'
-    Array.prototype.forEach.call(user.groups, function(value) {
-      tmp = value.replace('"', token);
-      filtered += '"'+value.replace('"', token)+'"';
-    });
+    //
+    // Note that this abuses the `amr` value (see https://tools.ietf.org/html/rfc8176) as these are not listed as
+    // registered values, and that groups are not authentication methods.
 
-    context.idToken['amr'] = filtered;
+    context.idToken['amr'] = user.groups || [];
   }
   return callback(null, user, context);
 }
