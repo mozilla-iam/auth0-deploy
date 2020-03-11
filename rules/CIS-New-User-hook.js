5ef4e8b7636d9feed75c59d89e80b719c4a129dd
April King <april@mozilla.com>
2020-03-11 12:17:57 -0500

    Fix errors and clean up CIS new user hook, add test for it

--- rules/CIS-New-User-hook.js
+++ rules/CIS-New-User-hook.js
@@ -1,40 +1,57 @@
-/*jshint esversion: 6 */
-
 function (user, context, callback) {
-  const ACCESS_KEY_ID = configuration.CIS_access_key_id;
-  const SECRET_KEY = configuration.CIS_access_secret_key;
-  let now = new Date();
-  let created = new Date(user.created_at);
+  const AWS_TIMEOUT = 10000;  // 10 seconds
+
+  if (!('CIS_access_key_id' in configuration) ||
+      !('CIS_access_secret_key' in configuration) ||
+      !('CIS_hook_arn' in configuration)) {
+    console.error(`Error: CIS hook configuration values are missing.`);
+    return callback(null, user, context);
+  }
+
+  const now = new Date();
+  const created = new Date(user.created_at);
 
   // User is LDAP? Bail because the user is created by the LDAP publisher
-  if (context.connection.startsWith("Mozilla-LDAP")) {
+  // TODO: Should this use context.connectionStrategy, like force-ldap-logins-over-ldap?
+  if (context.connection.startsWith('Mozilla-LDAP')) {
     return callback(null, user, context);
   }
+
   // User is older than 20 seconds? Bail - we only process newly added users
-  if ((now-created)/1000 > 20) {
+  if ((now - created) / 1000 > 20) {
     return callback(null, user, context);
   }
 
-  let AWS = require('aws-sdk');
-  var lambda = new AWS.Lambda({
+  const AWS = require('aws-sdk');
+  const lambda = new AWS.Lambda({
+    accessKeyId: configuration.CIS_access_key_id,
     apiVersion: '2015-03-31',
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_KEY,
+    httpOptions: {
+      connectTimeout: AWS_TIMEOUT,
+      timeout: AWS_TIMEOUT,
+    },
+    logger: console,
     region: 'us-west-2',
-    logger: console
+    secretAccessKey: configuration.CIS_access_secret_key,
   });
 
- // We invoke async (Event) as to not block login flow
- // See also https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#invoke-property
- var params = {FunctionName: configuration.CIS_hook_arn,
-               InvocationType: "Event", // Change to RequestResponse to block / debugging
-               Payload: `["${user.user_id}"]`,
-               LogType: "None" // Change to Tail for debugging
+  // We invoke async (Event) as to not block login flow
+  // See also https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#invoke-property
+  const params = {
+    FunctionName: configuration.CIS_hook_arn,
+    InvocationType: 'Event',  // Change to RequestResponse to block / debugging
+    Payload: `["${user.user_id}"]`,
+    LogType: 'None',          // Change to Tail for debugging
   };
-  lambda.invoke(params, function(err, data) {
-         if (err) {
-            console.log(`Error ${err}`);
-         }
-  });
-  return callback(null, user, context);
+
+  return lambda.invoke(params)
+    .promise()
+    .then(data => {
+      console.log(`Successfully called CIS new user hook for user_id ${user.user_id}`);
+      return callback(null, user, context);
+    })
+    .catch(error => {
+      console.error(`Error: Unable to invoke CIS new user lambda hook: ${error}`);
+      return callback(null, user, context);
+    });
 }
