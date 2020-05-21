9575efedf4f50bc3c20b6bf3268cbd8964856c42
Gene Wood <gene_wood@cementhorizon.com>
2020-05-21 11:10:53 -0700

    Add Stripe subplat saml mapping support
    https://bugzilla.mozilla.org/show_bug.cgi?id=1637117

--- rules/SAML-configuration-mapping.js
+++ rules/SAML-configuration-mapping.js
@@ -2,6 +2,7 @@ function (user, context, callback) {
   const CLIENTS = {
     'wgh8S9GaE7sJ4i0QrAzeMxFXgWZYtB0l': 'sage-intacct',  // Sage Intacct
     'R4djNlyXSl3i8N2KXWkfylghDa9kFQ84': 'thinksmart',    // mozilla.tap.thinksmart.com
+    'CLIENTID WILL GO HERE': 'stripe-subplat',  // name of RP will go here
   }
   const client = CLIENTS[context.clientID];
 
@@ -21,7 +22,7 @@ function (user, context, callback) {
       context.samlConfiguration.mappings = {
         'Company Name': 'company_name',
         'emailAddress': 'email',
-        'name':         'name',
+        'name': 'name',
       };
 
       break;
@@ -33,6 +34,33 @@ function (user, context, callback) {
       };
 
       break;
+    case 'stripe-subplat':
+      // https://bugzilla.mozilla.org/show_bug.cgi?id=1637117
+      var groupToStripeRoleMap = {
+        //  LDAP group name
+        'stripe_subplat_admin': [
+          //   stripe_role_name           stripe_account_id
+          {'role': 'admin', 'account': 'acct_1EJOaaJNcmPzuWtR'}],
+        'stripe_subplat_developer': [
+          {'role': 'developer', 'account': 'acct_1EJOaaJNcmPzuWtR'}],
+        'stripe_subplat_supportsp': [
+          {'role': 'support_specialist', 'account': 'acct_1EJOaaJNcmPzuWtR'}],
+        'stripe_subplat_analyst ': [
+          {'role': 'analyst', 'account': 'acct_1EJOaaJNcmPzuWtR'}],
+        'stripe_subplat_viewonly ': [
+          {'role': 'view_only', 'account': 'acct_1EJOaaJNcmPzuWtR'}]
+      };
+      context.samlConfiguration = context.samlConfiguration || {};
+      context.samlConfiguration.mappings = context.samlConfiguration.mappings || {};
+      Object.keys(groupToStripeRoleMap).forEach((groupName) => {
+        if (user.hasOwnProperty('groups') && user.groups.includes(groupName)) {
+          Object.keys(groupToStripeRoleMap[groupName]).forEach((roleInfo) => {
+            user.app_metadata[roleInfo['account']] = roleInfo['role'];
+          });
+          context.samlConfiguration.mappings['Stripe-Role-' + roleInfo['account']] = 'app_metadata.' + roleInfo['account'];
+        }
+      });
+    }
   }
 
   return callback(null, user, context);
