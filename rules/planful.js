0f780756cf9a3139108495bdd751098f876f8f24
Rob Tucker <rtucker@mozilla.com>
2021-04-19 15:13:46 -0400

    Adding planful static idp string, but direct to user attribute

--- rules/planful.js
+++ rules/planful.js
@@ -1,7 +1,6 @@
 function (user, context, callback) {
   // Planful requires a value for each user which is always the same
   const ALLOWED_CLIENTIDS = [
-    'H5ddlJSCfGP8ab65EnWaB2sd541CJAlM',  // Planful
   ];
 
   // We only care about the above clients
@@ -9,8 +8,7 @@ function (user, context, callback) {
     return callback(null, user, context);
   }
 
-  user.app_metadata = user.app_metadata || {};
-  user.app_metadata["IdP Entity ID"] = "fed.identropy.hostanalytics.saml2"
+  user["IdP Entity ID"] = "fed.identropy.hostanalytics.saml2"
 
 
   return callback(null, user, context);
