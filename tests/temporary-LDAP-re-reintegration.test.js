3557e26b67441a381e661940e1b2db876b033b99
Jake Watkins <jwatkins@mozilla.com>
2022-11-04 19:22:59 -0400

    Add auth0 mock module to test

--- tests/temporary-LDAP-re-reintegration.test.js
+++ tests/temporary-LDAP-re-reintegration.test.js
@@ -4,6 +4,7 @@ const configuration = require('./modules/global/configuration.js');
 const context = require('./modules/contexts/context.js');
 const Global = require('./modules/global/global.js');
 const user = require('./modules/users/user.js');
+const auth0 = require('./modules/global/auth0.js');
 
 const loader = require('./modules/rule-loader.js');
 const rule = loader.load('temporary-LDAP-re-reintegration.js', false);
@@ -18,7 +19,7 @@ beforeEach(() => {
 
 
 test('Test Placeholder', () => {
-  output = rule(_user, _context, configuration, Global);
+  output = rule(_user, _context, configuration, Global, auth0);
 
   expect(true).toEqual(true);
 });
