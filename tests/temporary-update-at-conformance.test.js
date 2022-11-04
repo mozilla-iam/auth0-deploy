1877e459e6586f0ea0153db244dfdbed26157d9a
Jake Watkins <jwatkins@mozilla.com>
2022-11-04 19:13:20 -0400

    Fix dup function name and add missing test suites

---
+++ tests/temporary-update-at-conformance.test.js
@@ -0,0 +1,26 @@
+const _ = require('lodash');
+
+const configuration = require('./modules/global/configuration.js');
+const context = require('./modules/contexts/context.js');
+const Global = require('./modules/global/global.js');
+const user = require('./modules/users/user.js');
+
+const loader = require('./modules/rule-loader.js');
+const rule = loader.load('temporary-update-at-conformance.js', false);
+
+
+// jest setup to reset _user and _context, preventing tests from writing to objects
+beforeEach(() => {
+  _user = _.cloneDeep(user);
+  _context = _.cloneDeep(context);
+  output = undefined;
+});
+
+
+test('Test Placeholder', () => {
+  // Inject the empty AAI array attribute.  This is normally setup in the aai.js rule
+  _user.aai = [];
+  output = rule(_user, _context, configuration, Global);
+
+  expect(true).toEqual(true);
+});
