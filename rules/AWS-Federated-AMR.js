9704ccb93f2b72da0b087083f2a829834f5bbf35
atoll <atoll@mozilla.com>
2023-07-25 10:25:04 -0700

    Populate the id token as a /groupname/ string. Remove oaud token.

--- rules/AWS-Federated-AMR.js
+++ rules/AWS-Federated-AMR.js
@@ -81,12 +81,12 @@ function AWSFederatedAMR(user, context, callback) {
       let aws_groups = Object.keys(global.awsGroupRoleMap);
       user.groups = user.groups || [];
       context.idToken.amr = groupIntersection(user.groups, aws_groups);
-      context.idToken.oaud = groupIntersection(user.groups, aws_groups);
-      context.idToken.id = groupIntersection(user.groups, aws_groups);
+      //context.idToken.oaud = groupIntersection(user.groups, aws_groups);
+      context.idToken.id = '/'.concat(groupIntersection(user.groups, aws_groups).join('/'), '/');
       console.log(`User groups: ${user.groups.join(", ")}`);
       console.log(`AWS groups: ${aws_groups.join(", ")}`);
       console.log(`idToken.amr: Intersection of user groups and AWS groups: ${context.idToken.amr}`);
-      console.log(`idToken.oaud: Intersection of user groups and AWS groups: ${context.idToken.oaud}`);
+      //console.log(`idToken.oaud: Intersection of user groups and AWS groups: ${context.idToken.oaud}`);
       console.log(`idToken.id: Intersection of user groups and AWS groups: ${context.idToken.id}`);
 
       // If Auth0 is going to send the user to Duo, Auth0 will modify the `amr` claim. Auth0 will specifically
