3d48323f4aa9b221d3e5babc8bb0f22e4fa5cd02
Jake Watkins <jwatkins@mozilla.com>
2024-02-15 14:16:49 -0500

    Expand test coverage for SAML-Braintree-attribute rule

--- tests/SAML-Braintree-attribute.test.js
+++ tests/SAML-Braintree-attribute.test.js
@@ -6,7 +6,7 @@ const Global = require('./modules/global/global.js');
 const user = require('./modules/users/user.js');
 
 const loader = require('./modules/rule-loader.js');
-const rule = loader.load('SAML-Braintree-attribute.js', false);
+const rule = loader.load('SAML-Braintree-attribute.js', true);
 
 
 // jest setup to reset _user and _context, preventing tests from writing to objects
@@ -16,9 +16,25 @@ beforeEach(() => {
   output = undefined;
 });
 
+const clientIDs = ['x7TF6ZtJev4ktoHR4ObWmA9KeqGni6rq'];
 
-test('Test Placeholder', () => {
-  output = rule(_user, _context, configuration, Global);
+describe('Ensure rule does not apply when clientID does not match', () => {
+  test('Rule does not change context object', () => {
+    output = rule(_user, _context, configuration, Global);
 
-  expect(true).toEqual(true);
+    expect(output.context).toEqual(context);
+    expect(output.user).toEqual(user);
+  });
+});
+
+describe('Ensure multiple clientID coverage', () => {
+  test.each(clientIDs)('Ensure SAML configuration mappings for client %s', (clientID) => {
+    _context.clientID = clientID;
+    output = rule(_user, _context, configuration, Global);
+
+    expect(output.context.samlConfiguration.mappings).toEqual({
+      'grant_all_merchant_accounts': 'grant_all_merchant_accounts',
+      'roles': 'app_metadata.groups',
+    });
+  });
 });
