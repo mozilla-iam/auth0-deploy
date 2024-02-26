ec9a8ff957f472c507f6e3c8214da0da64d8e5c2
Jake Watkins <jwatkins@mozilla.com>
2024-02-26 16:15:24 -0500

    Expand test coverage for Global-Function-Declarations rule

--- tests/Global-Function-Declarations.test.js
+++ tests/Global-Function-Declarations.test.js
@@ -1,5 +1,8 @@
 const _ = require('lodash');
 
+const querystring = require('querystring');
+const jwt = require('jsonwebtoken');
+
 const configuration = require('./modules/global/configuration.js');
 const context = require('./modules/contexts/context.js');
 const Global = require('./modules/global/global.js');
@@ -9,16 +12,83 @@ const loader = require('./modules/rule-loader.js');
 const rule = loader.load('Global-Function-Declarations.js', false);
 
 
+// Simple URL format string validator
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+
 // jest setup to reset _user and _context, preventing tests from writing to objects
 beforeEach(() => {
   _user = _.cloneDeep(user);
   _context = _.cloneDeep(context);
   output = undefined;
+  // reset rcontext between tests
+  rcontext = undefined;
+  // delete the postError function so the Global rule can actually set it
+  delete Global.postError;
 });
 
 
-test('Test Placeholder', () => {
+test('Expect context and user objects to be unchanged', () => {
   output = rule(_user, _context, configuration, Global);
 
-  expect(true).toEqual(true);
+  expect(output.user).toEqual(_user);
+  expect(output.context).toEqual(_context);
+});
+
+describe('Test continue endpoint', () => {
+  test('Expect an UnauthorizedError to be raised if context.protocol is redirect-callback', () => {
+    _context.protocol = "redirect-callback";
+    output = rule(_user, _context, configuration, Global);
+
+    expect(output._.name).toEqual("UnauthorizedError");
+  });
+
+});
+
+describe('Test postError function', () => {
+  test('Expect global.postError to be set as a function', () => {
+    output = rule(_user, _context, configuration, Global);
+
+    expect(typeof(output.global.postError) === 'function').toEqual(true);
+  });
+
+
+  test('Expect postError to set context.redirect.url with a valid URL', () => {
+    output = rule(_user, _context, configuration, Global);
+    rcontext = output.global.postError('testing', _context);
+
+    expect(isValidUrl(rcontext.redirect.url)).toEqual(true);
+  });
+
+  test('Expect postError to set context.redirect.url with a set URL prefix', () => {
+    output = rule(_user, _context, configuration, Global);
+    rcontext = output.global.postError('testing', _context);
+
+    expect(rcontext.redirect.url.startsWith("https://sso.mozilla.com/forbidden?error=")).toEqual(true);
+  });
+
+  test('Expect postError redirect.url to contain a valid jsonwebtoken', () => {
+    output = rule(_user, _context, configuration, Global);
+
+    rcontext = output.global.postError('testing', _context);
+    // parse the redirect URL
+    const parsedUrl = new URL(rcontext.redirect.url);
+    // parse the query parameters
+    const queryParams = querystring.parse(parsedUrl.search.slice(1));
+    // get the jsonwebtoken to be verifed
+    const token = queryParams.error;
+    // get public key to decode jsonwebtoken
+    const pkey = Buffer.from(configuration.jwt_msgs_rsa_pkey, 'base64').toString('ascii');
+    // decode the token
+    const decoded = jwt.verify(token, pkey);
+
+    expect(decoded.code === 'testing').toEqual(true);
+  });
 });
